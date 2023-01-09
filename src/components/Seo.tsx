2ca4631cfbd1e8cb5a2264ed7eccfb326b7d1323
Daryl <dyk0203@naver.com>
2023-01-10 02:19:51 +0900

    useStaticQuery

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
 interface ISeoProps {
@@ -5,7 +6,20 @@ interface ISeoProps {
 }
 
 const Seo = ({ title }: ISeoProps) => {
-  return <title>{title} | DevStickers!</title>;
+  const data = useStaticQuery<Queries.SeoDataQuery>(graphql`
+    query SeoData {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <title>
+      {title} | {data?.site?.siteMetadata?.title}
+    </title>
+  );
 };
 
 export default Seo;
