68b7233f9955639bd8b8fe9c0d2d7104517517e8
Daryl <dyk0203@naver.com>
2023-01-10 02:29:00 +0900

    Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,30 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-const Blog = () => {
+const Blog = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
+  console.log(data);
   return (
     <Layout title="Blog">
-      <p>The most recent news from my shop</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
 
